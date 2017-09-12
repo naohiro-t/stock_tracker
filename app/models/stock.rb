2d69312d9f11583aa4e535cf4baaa91ee8379a79
naohiro-t <takahashi.naohiro1222@gmail.com>
2017-09-12 14:19:14 +0900

    add methods to stock.rb

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,32 @@
-class Stock < ApplicationRecord
+class Stock < ActiveRecord::Base
 
+  # this method is to query data from db
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+  # this method is available without create instance
+  def self.new_from_lookup(ticker_symbol)
+    # look up stock info, store it in lookup_stock
+    lookup_stock = StockQuote::Stock.quote(ticker_symbol)
+
+    # if name is invalid return nil
+    return nil unless lookup_stock.name
+
+    new_stock = new(ticker: lookup_stock.symbol, name: lookup_stock.name)
+    new_stock.last_price = new_stock.price
+
+    new_stock
+  end
+
+  # get stock price
+  def price
+    last_price = StockQuote::Stock.quote(ticker).close
+    # if closing price is nil, get open price
+    if !last_price
+      last_price = StockQuote::Stock.quote(ticker).open
+    end
+    return last_price
+  end
 
 end
