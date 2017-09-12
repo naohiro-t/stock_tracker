325a6e2ad29d01bb26c7317107047d42371fc4c1
naohiro-t <takahashi.naohiro1222@gmail.com>
2017-09-12 18:39:07 +0900

    fix typo, but still results dont show up

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,10 +1,10 @@
 var init_stock_lookup;
 
-init_stock_lookup = function() {
+init_stock_lookup = function(){
     $('#stock-lookup-form').on('ajax:success', function(event, data, status){
         $('#stock-lookup').replaceWith(data);
         init_stock_lookup();
-    })
+    });
 }
 
 
