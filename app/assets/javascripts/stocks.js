45fd1fc8d7053ae08998c316b2d0aef39e4b624d
naohiro-t <takahashi.naohiro1222@gmail.com>
2017-09-12 21:24:37 +0900

    add error handling for ajax, but stock search result doesn't still show up

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -4,7 +4,12 @@ init_stock_lookup = function(){
     $('#stock-lookup-form').on('ajax:success', function(event, data, status){
         $('#stock-lookup').replaceWith(data);
         init_stock_lookup();
-    })
+    });
+    //handle error
+    $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+        $('#stock-lookup-results').replaceWith('');
+        $('stock-lookup-errors').replaceWith('Stock was not found');
+    });
 }
 
 
