02765c25e35c07bc708002d1bc105330a1f08137
naohiro-t <takahashi.naohiro1222@gmail.com>
2017-09-13 16:05:06 +0900

    add restriction for registering my stocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+
+  def can_add_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_added?(ticker_symbol)
+  end
+
+  # check how many stocks are registered for the user
+  def under_stock_limit?
+    user_stocks.count < 10
+  end
+
+  def stock_already_added?(ticker_symbol)
+    stock = Stock.find_by_ticker(ticker_symbol)
+    if stock
+      true
+    else
+      false
+    end
+  end
+
 end
