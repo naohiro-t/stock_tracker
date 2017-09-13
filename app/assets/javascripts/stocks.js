2a3d0d80dd2c2b91912451218c13410d02c557b2
naohiro-t <takahashi.naohiro1222@gmail.com>
2017-09-13 10:22:28 +0900

    modify stocks.js, and results show up now

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,8 +1,7 @@
 var init_stock_lookup;
-
 init_stock_lookup = function(){
-    $('#stock-lookup-form').on('ajax:success', function(event, data, status){
-        $('#stock-lookup').replaceWith(data);
+    $('#stock-lookup-form').on('ajax:success', function(event, data, status, xhr){
+        $('#stock-lookup').replaceWith(event.detail[2].responseText);
         init_stock_lookup();
     });
     //handle error
@@ -10,9 +9,27 @@ init_stock_lookup = function(){
         $('#stock-lookup-results').replaceWith('');
         $('stock-lookup-errors').replaceWith('Stock was not found');
     });
-}
-
-
+};
 $(document).ready(function(){
     init_stock_lookup();
-})
+});
+
+// These are previous code which doesn't work as I expect.
+// var init_stock_lookup;
+//
+// init_stock_lookup = function(){
+//     $('#stock-lookup-form').on('ajax:success', function(event, data, status){
+//         $('#stock-lookup').replaceWith(data);
+//         init_stock_lookup();
+//     });
+//     //handle error
+//     $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+//         $('#stock-lookup-results').replaceWith('');
+//         $('stock-lookup-errors').replaceWith('Stock was not found');
+//     });
+// }
+//
+//
+// $(document).ready(function(){
+//     init_stock_lookup();
+// })
